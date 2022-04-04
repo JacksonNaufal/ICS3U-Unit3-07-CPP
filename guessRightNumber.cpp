386a43bf4bd7885f69391b8546608de42fa95f5a
Jackson Naufal <Jackson.naufal@mths.ca>
2022-04-04 23:11:51 +0000

    GrandmaAgeGood

--- guessRightNumber.cpp
+++ guessRightNumber.cpp
@@ -11,29 +11,26 @@
 
 int main() {
     // this function is a random number guesser
-    int guessNumber;
-    int randomNumber;
-
-
-    std::random_device rseed;
-    std::mt19937 rgen(rseed());  // mersenne_twister
-    std::uniform_int_distribution<int> idist(0, 9);  // [0,9]
-    randomNumber = idist(rgen);
+    std::string age;
+    int integerAsNumber;
 
 
     // input
-    std::cout << "Enter your random number (0-9): ";
-    std::cin >> guessNumber;
-    std::cout << "" << std::endl;
-    // process
-    if (guessNumber != randomNumber) {
-    // output
-        std::cout << "Guess is incorrect";
+    std::cout << "Enter your age!: ";
+    std::cin >> age;
+    std::cout << std::endl;
+    
+    // process & output
+    try {
+        integerAsNumber = std::stoi(age);
+        
+    if (integerAsNumber >= 25 && integerAsNumber <= 40 ) {
+        std::cout << "You're a good age";
+    } else {
+        std::cout << "You're not a good age";
     }
-    // process
-    if (guessNumber == randomNumber) {
-    // output
-        std::cout << "Guess is correct";
+    } catch (std::invalid_argument) {
+        std::cout << "Invalid Input.";
     }
     std::cout << "" << std::endl;
     std::cout << "\nDone." << std::endl;
